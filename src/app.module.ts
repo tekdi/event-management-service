ae8e9f5fce0b865e0ee270941bf472eb5c975e16
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2024-07-11 11:01:51 +0530

    Task #222957 : chore - removed cohort and users entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { EventModule } from './modules/event/event.module';
 import { DatabaseModule } from './common/database-modules';
 import { AttendeesModule } from './modules/attendees/attendees.module';
 
-
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), EventModule, DatabaseModule, AttendeesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    EventModule,
+    DatabaseModule,
+    AttendeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
