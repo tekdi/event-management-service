8f2d64ddaa3a8b58628d81ea9747283c031e254d
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2024-07-12 18:20:49 +0530

    Task #223114 chore: added global exception filter

---
+++ src/common/filters/exception.filter.ts
@@ -0,0 +1,29 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Response, Request } from 'express';
+import APIResponse from '../utils/response';
+import { ERROR_MESSAGES } from '../utils/constants.util';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+    constructor(private readonly apiId?: string) { }
+
+    catch(exception: unknown, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const status = exception instanceof HttpException ? exception.getStatus() : 500;
+        const exceptionResponse = exception instanceof HttpException ? exception.getResponse() : null;
+        const errorMessage =
+            exception instanceof HttpException
+                ? (exceptionResponse as any).message || exception.message
+                : ERROR_MESSAGES.INTERNAL_SERVER_ERROR;
+        const detailedErrorMessage = `${errorMessage}`;
+        console.log('detailedErrorMessage', detailedErrorMessage);
+        const errorResponse = APIResponse.error(
+            this.apiId,
+            detailedErrorMessage,
+            exception instanceof HttpException ? exception.name : ERROR_MESSAGES.INTERNAL_SERVER_ERROR, // error
+            status.toString(),
+        );
+        return response.status(status).json(errorResponse);
+    }
+}
