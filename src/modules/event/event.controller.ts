634c04be0fa59075431f1219ce638ff1a29a456f
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2024-07-12 18:27:24 +0530

    Task #223114 chore: Constants for api messages

--- src/modules/event/event.controller.ts
+++ src/modules/event/event.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, Res, ValidationPipe, BadRequestException, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, Res, ValidationPipe, BadRequestException, ParseUUIDPipe, UseFilters } from '@nestjs/common';
 import { EventService } from './event.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
@@ -13,65 +13,76 @@ import {
 } from '@nestjs/swagger';
 import { Response } from 'express';
 import { SearchFilterDto } from './dto/search-event.dto';
-import { DateValidationPipe, DeadlineValidationPipe, ParamsValidationPipe } from 'src/common/pipes/event-validation.pipe';
+import { DateValidationPipe, RegistrationDateValidationPipe, ParamsValidationPipe } from 'src/common/pipes/event-validation.pipe';
+import { ConfigService } from '@nestjs/config';
+import { AllExceptionsFilter } from 'src/common/filters/exception.filter';
+import { API_ID, ERROR_MESSAGES, SUCCESS_MESSAGES } from 'src/common/utils/constants.util';
 
 @Controller('event/v1')
 @ApiTags('Create Event')
 export class EventController {
-  constructor(private readonly eventService: EventService) { }
+  constructor(private readonly eventService: EventService,
+    private readonly configService: ConfigService,
+  ) { }
 
+  @UseFilters(new AllExceptionsFilter(API_ID.CREATE_EVENT))
   @Post('/create')
   @ApiBody({ type: CreateEventDto })
-  @UsePipes(new DateValidationPipe, new DeadlineValidationPipe, new ParamsValidationPipe, new ValidationPipe({ transform: true }))
+  @UsePipes(new ValidationPipe({ transform: true }), new DateValidationPipe, new RegistrationDateValidationPipe, new ParamsValidationPipe)
   @ApiCreatedResponse({
-    description: 'Created Event',
+    description: SUCCESS_MESSAGES.EVENT_CREATED,
   })
-  @ApiBadRequestResponse({ description: 'Invalid request' })
-  @ApiInternalServerErrorResponse({ description: 'Server Error.' })
+  @ApiBadRequestResponse({ description: ERROR_MESSAGES.INVALID_REQUEST_BODY })
+  @ApiInternalServerErrorResponse({ description: ERROR_MESSAGES.INTERNAL_SERVER_ERROR })
   async create(@Body() createEventDto: CreateEventDto, @Res() response: Response,) {
     const userId = '016badad-22b0-4566-88e9-aab1b35b1dfc'; // later come from JWT-token
+    this.configService;
     return this.eventService.createEvent(createEventDto, userId, response);
   }
 
+  @UseFilters(new AllExceptionsFilter(API_ID.GET_EVENTS))
   @Post('/list')
   @ApiBody({ type: SearchFilterDto })
-  @ApiInternalServerErrorResponse({ description: 'Server Error.' })
+  @ApiInternalServerErrorResponse({ description: ERROR_MESSAGES.INTERNAL_SERVER_ERROR })
   @UsePipes(new ValidationPipe({ transform: true }))
   @ApiOkResponse({
     description: 'Searched',
     status: 200
   })
   async findAll(@Res() response: Response, @Body() requestBody: SearchFilterDto) {
-    return this.eventService.getEvents(response, requestBody);
+    // return this.eventService.getEvents(response, requestBody);
   }
 
+  @UseFilters(new AllExceptionsFilter(API_ID.GET_EVENT_BY_ID))
   @Get('/:id')
   @ApiOkResponse({
     description: 'Get event details by id',
     status: 200
   })
-  @ApiInternalServerErrorResponse({ description: 'Server Error.' })
+  @ApiInternalServerErrorResponse({ description: ERROR_MESSAGES.INTERNAL_SERVER_ERROR })
   findOne(@Param('id', ParseUUIDPipe) id: string, @Res() response: Response) {
-    return this.eventService.getEventByID(id, response);
+    // return this.eventService.getEventByID(id, response);
   }
 
+  @UseFilters(new AllExceptionsFilter(API_ID.UPDATE_EVENT))
   @Patch('/:id')
   @ApiBody({ type: UpdateEventDto })
-  @ApiResponse({ status: 200, description: 'Event updated successfully' })
-  @ApiInternalServerErrorResponse({ description: 'Server Error.' })
+  @ApiResponse({ status: 200, description: SUCCESS_MESSAGES.EVENT_UPDATED })
+  @ApiInternalServerErrorResponse({ description: ERROR_MESSAGES.INTERNAL_SERVER_ERROR })
   @UsePipes(new ValidationPipe({ transform: true }))
   updateEvent(@Param('id', ParseUUIDPipe) id: string, @Body() updateEventDto: UpdateEventDto, @Res() response: Response) {
     if (!updateEventDto || Object.keys(updateEventDto).length === 0) {
-      throw new BadRequestException('Please do not pass empty body')
+      throw new BadRequestException(ERROR_MESSAGES.INVALID_REQUEST_BODY)
     }
     const userId = '01455719-e84f-4bc8-8efa-7024874ade08'; // later come from JWT-token
-    return this.eventService.updateEvent(id, updateEventDto, userId, response);
+    // return this.eventService.updateEvent(id, updateEventDto, userId, response);
   }
 
+  @UseFilters(new AllExceptionsFilter(API_ID.DELETE_EVENT))
   @Delete('/:id')
-  @ApiResponse({ status: 200, description: 'Event deleted successfully' })
-  @ApiResponse({ status: 404, description: 'Event not found' })
+  @ApiResponse({ status: 200, description: SUCCESS_MESSAGES.EVENT_DELETED })
+  @ApiResponse({ status: 404, description: SUCCESS_MESSAGES.EVENT_NOT_FOUND })
   deleteEvent(@Param('id', ParseUUIDPipe) id: string, @Res() response: Response) {
-    return this.eventService.deleteEvent(id, response);
+    // return this.eventService.deleteEvent(id, response);
   }
 }
