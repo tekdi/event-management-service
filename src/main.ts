0957c15faa8cbf36c886efd8673d0c532075a5f7
poojakarma <pooja.karma@tekditechnologies.com>
2024-08-12 11:54:10 +0530

    resolve issue of swagger

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  // app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('event-service');
   const options = new DocumentBuilder()
     .setTitle('Event Management')
     .setDescription('CRUD API')
