b10f89123e1037c78e1112f26543ba6b62d5698a
poojakarma <pooja.karma@tekditechnologies.com>
2024-08-13 13:00:09 +0530

    change in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('event-service');
+  // app.setGlobalPrefix('event-service');
   const options = new DocumentBuilder()
     .setTitle('Event Management')
     .setDescription('CRUD API')
