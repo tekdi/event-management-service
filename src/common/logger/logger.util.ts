e2970ea412a8b6c16233dd57982a78ec85a4dd20
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2024-11-05 18:20:07 +0530

    chore : stop log on console

--- src/common/logger/logger.util.ts
+++ src/common/logger/logger.util.ts
@@ -25,7 +25,6 @@ export class LoggerWinston {
           customFormat,
         ),
         transports: [
-          new winston.transports.Console(),
           new winston.transports.File({
             filename: 'error.log',
             level: 'error',
