16c039fa9696683512b3951ddf9bd91e96cced2f
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2025-02-10 17:51:16 +0530

    chore : removed userId from query params

--- src/modules/attendance/attendance.controller.ts
+++ src/modules/attendance/attendance.controller.ts
@@ -9,34 +9,28 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { Response, Request } from 'express';
-import { ApiBody, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiBasicAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 import { AttendanceService } from './attendance.service';
 import { AllExceptionsFilter } from 'src/common/filters/exception.filter';
 import { MarkMeetingAttendanceDto } from './dto/markAttendance.dto';
-import { checkValidUserId } from 'src/common/utils/functions.util';
-import { API_ID, ERROR_MESSAGES } from 'src/common/utils/constants.util';
+import { API_ID } from 'src/common/utils/constants.util';
+import { GetUserId } from 'src/common/decorators/userId.decorator';
 
 @Controller('attendance/v1')
 @ApiTags('Event-Attendance')
+@ApiBasicAuth('access-token')
 export class EventAttendance {
   constructor(private readonly attendanceService: AttendanceService) {}
 
   @UseFilters(new AllExceptionsFilter(API_ID.MARK_EVENT_ATTENDANCE))
   @Post('/markeventattendance')
   @ApiBody({ type: MarkMeetingAttendanceDto })
-  @ApiQuery({
-    name: 'userId',
-    required: true,
-    description: ERROR_MESSAGES.USERID_REQUIRED,
-    example: '123e4567-e89b-12d3-a456-426614174000',
-  })
   @UsePipes(new ValidationPipe({ transform: true }))
   async markEventAttendance(
     @Body() markZoomAttendanceDto: MarkMeetingAttendanceDto,
     @Res() response: Response,
-    @Req() request: Request,
+    @GetUserId() userId: string,
   ): Promise<Response> {
-    const userId: string = checkValidUserId(request.query?.userId);
     return this.attendanceService.markAttendanceForMeetingParticipants(
       markZoomAttendanceDto,
       userId,
